6fff9a36d9878eed3dd1fc8d0d17b03db7b4f288
Hamza Shabbir <hamzashabbir447@gmail.com>
2022-03-04 12:50:11 +0500

    Circular list added

--- Assignment1/Circular List.py
+++ Assignment1/Circular List.py
@@ -1,17 +1,15 @@
 # Circular List 
-l=[1,2,3,4,5]
-l_copy=l
-b=l_copy[1]
-#l[0]=l[-1]
+from copy import copy, deepcopy
+l=[1,2,3,4,5,6,7]
+A=l.copy()
+
 a=len(l)
-#for i in range(1,a):
+l[0]=A[-1]
+for i in range(1,a):
+    l[i]=A[i-1]
 
-buffer=l[1]
-l[0]=l[-1]
-l[4]=l[3]
-l[3]=l[2]
-l[2]=l[1]
-l[1]=l[0]
-
+    
 print(l)
+
+
 
