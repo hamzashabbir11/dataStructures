9fabada4d63e63bae5e85e5ed134c2efe9702766
Hamza Shabbir <hamzashabbir447@gmail.com>
2022-03-05 21:28:30 +0500

    feature added

--- Assignment1/Inventory Management.py
+++ Assignment1/Inventory Management.py
@@ -1,21 +1,30 @@
-from itertools import product
 print("Enter your Product Items and their Price, when done enter 'exit' ")
 shop={ 
 }
-
 product_list=[]
 price_list=[]
 for i in range(20):
 
     product=input('Enter Product Name')
-    if product=='exit'.lower:
+    if product=='exit':
         break
     product_list.append(product)
-    price=input('Enter Price in usd')
+    price=int(input('Enter Price in usd'))
     price_list.append(price)
     shop[product_list[i]]=price_list[i]
-
 print(shop)
+price_usd=input("Enter dollar amount")
+#print(*shop) # prints all the keys 
+'''product_list=['a','b','c']
+price_list=[1,2,3]'''
+for i in range(len(product_list)):
+    if price_list[i]<price_usd:
+        print(product_list[i])
+        
+   
+
+
+   
 
 
     
