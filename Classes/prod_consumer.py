fa35cab8bc7bced683f02a80453181f2c52c800d
Hamza Shabbir <hamzashabbir447@gmail.com>
2022-03-12 13:46:47 +0500

    weekend commits

--- Classes/prod_consumer.py
+++ Classes/prod_consumer.py
@@ -1,23 +1,24 @@
+import threading
 import time
-order_producer=['Pizza','McCrispy','McFries','Crosoint','Latte','Club Sandwitch','French Toast','Steak','Milk Shake','Cake']
+order_producer=['Pizza','McCrispy','McFries','Crosont','Doughnut']
 order=[]
 j=0
 i=0
-
-while(i<len(order_producer)):
-    order.insert(i,order_producer[i])
-    print(order[i])
-    time.sleep(0.5)
-    i=i+1
-
 serve=[]
 
+      
+while(i<len(order_producer)):
+        order.insert(i,order_producer[i])
+        print(order[i])
+        time.sleep(0.5)
+        i=i+1
+
+      
 print('Your Order is Preparing ')
 while(j<len(order)):
     print(order.pop(j))
     time.sleep(2)
-    
-    
+
 
 print(order)
 
